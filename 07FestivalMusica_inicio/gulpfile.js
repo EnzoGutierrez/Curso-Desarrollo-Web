e62eb0f24c4df373799e1334f9908865cc386366
EnzoGutierrez <gutierrezenzo911@gmail.com>
2022-08-15 15:38:54 -0300

    Cuarta pagina web

--- 07FestivalMusica_inicio/gulpfile.js
+++ 07FestivalMusica_inicio/gulpfile.js
@@ -4,6 +4,10 @@ const {src, dest, watch, parallel   } = require("gulp");
 //CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
 
 //Imagenes
 const cache = require("gulp-cache");
@@ -13,8 +17,11 @@ const avif = require("gulp-avif");
 
 function css(done){
     src("src/scss/**/*.scss")    //Indentificar el archivo SASS
+        .pipe(sourcemaps.init())
         .pipe(plumber()) //Compilarlo
         .pipe(sass())
+        .pipe(postcss([autoprefixer(), cssnano]))
+        .pipe(sourcemaps.write("."))
         .pipe(dest("build/css"))    //Almacenarla en el disco duro
 
     done();
